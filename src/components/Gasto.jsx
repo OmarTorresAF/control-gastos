8d2c7da82e6bd8e0df805aca11926eeb713998d3
TDevtechs <torresdevtech@gmail.com>
2024-01-16 11:38:28 -0600

    Component 'Gasto' Import 'Gasto' into 'ListadoGastos' and map it on the component so we can arrange an id.

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gasto = () => {
+  return (
+    <div>
+        <p>desde Gasto</p>
+    </div>
+  )
+}
+
+export default Gasto
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Gasto  from './Gasto'
 
 export const ListadoGastos = ({gastos}) => {
   return (
     <div className='listado-gastos contenedor'>
         <h2>{gastos.lenght ? 'gastos' : 'No hay gastos aun'}</h2>
+
+        {gastos.map( gasto => (
+            <Gasto
+            key={gasto.id}
+            gasto={gasto}
+            />
+        ))}
     </div>
   )
 }
