3766ba73f5377cfe87fceb2adc164f4ef241cefd
TDevtechs <torresdevtech@gmail.com>
2024-01-16 07:52:26 -0600

    Modal form with categories so you can pick which spends or use of money.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -26,6 +26,40 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
         </div>
         <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
           <legend>Nuevo Gasto</legend>
+          <div className='campo'>
+            <label htmlFor="nombre">Nombre del Gasto</label>
+            <input 
+            type="text"
+            id='nombre'
+            placeholder='Añade el gasto del gasto ej. 300' />
+
+          </div>
+          <div className='campo'>
+            <label htmlFor="nombre">Cantidad</label>
+            <input 
+            type="text"
+            id='nombre'
+            placeholder='Añade la cantidad del gasto ej. 300' />
+
+          </div>
+          <div className='campo'>
+            <label htmlFor="nombre">Categoria</label>
+            <select 
+            id="categoria">
+              <option value="">Seleccione</option>
+              <option value="ahorro">Ahorro</option>
+              <option value="ahorro">Comida</option>
+              <option value="ahorro">Casa</option>
+              <option value="ahorro">Gastos Varios</option>
+              <option value="ahorro">Ocio</option>
+              <option value="ahorro">Salud</option>
+              <option value="ahorro">Subscripciones</option>
+
+            </select>
+
+          </div>
+          <input type="submit" 
+          value= 'añadir gasto'/>
         </form>
     </div>
   )
